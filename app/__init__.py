840be469ba30d83c0a48e1fab3b3d1a6258c98c3
waimea-ammacintosh <ammacintosh@waimea.school.nz>
2025-05-15 12:14:43 +1200

    set up project with pages.

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 
 @app.get("/")
 def home ():
-    return render_template('pages/home.jinja')
+     return render_template('pages/home.jinja')
 
 @app.get("/random/")
 def random():
-    return str(randint(1,1000))
+     randNum = str(randint(1,1000))
+     return render_template('pages/random.jinja', number = randNum) #number = str(randint(1,1000)) would also work, the two lines are just to be ultra-clear.
 
 @app.get("/about/")
 def about():
-    return render_template('pages/about.jinja')
+     return render_template('pages/about.jinja')
